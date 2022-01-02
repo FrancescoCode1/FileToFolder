fb0882795ab17784bdf1deade08937904b0ae462
Francesco <71893290+FrancescoCode1@users.noreply.github.com>
2022-01-02 16:53:17 +0100

    Rewrote most of the logic

--- main.py
+++ main.py
@@ -2,39 +2,35 @@ import os
 from os import walk
 import shutil
 
-#creates a list of files ignoring folders
-filenames = next(walk("Directory"), (None, None, []))[2]
-original = r"directory"
+filenames = next(walk(r"C:\\Users\\Example"), (None, None, []))[2]
+original = r"C:\\Users\\Example"
 
-#I don't know what I did here. It just worked so I didn't change it.
+#checkFiletype: stores the value of rfind. if filetype exists it returns > -1 otherwise -1
+
+
 def main():
-    #loops through the list
     for filename in filenames:
-        #Making sure that it doesnt create a directory for directories
         if filename == os.path.isdir("directory"):
             pass
-        #should have worked with switch case statements. When i wrote the code Python didnt feature switch-case. 
-        #i have 2 file extensions. either mp4 or mkv. for either of those i want to create a directory and put them into the directory
         else:
             s = filename
-            cutName = s.rfind(".mkv") #rfind returns -1 if the string is not in the filename
-
-            if cutName > -1: 
-                cutName = filename[:-4]  #this takes the filename and removes the last 4 symbols. Basically cuts the file extension
-                dirName = (f"directory\\{cutName}") #stores the new name of the directory
+            checkFiletype = s.rfind(".mkv")
+            
+            if checkFiletype > -1:
+                filenameNoExt = filename[:-4]
+                dirName = (f"C:\\Users\\Example\\{filenameNoExt}")
                 os.mkdir(dirName)
-                shutil.move(original + f"\\" + s, dirName + f"\\" + s)
-
-            elif cutName == -1:
-                cutname = s.rfind(".mp4")
-                if cutname > -1:
-                    cutName = filename[:-4]
-                    dirName = (f"directory\\{cutName}")
+                shutil.move(original + f"\\" + s, dirName)
+            elif checkFiletype == -1:
+                mp4name = s.rfind(".mp4")
+                
+                if mp4name > -1:
+                    filenameNoExt = filename[:-4]
+                    dirName = (f"C:\\Users\\Example\\{filenameNoExt}")
                     os.mkdir(dirName)
-                    shutil.move(original + f"\\" + s, dirName + f"\\" + s)
-
-                elif cutname == -1:
+                    shutil.move(original + f"\\" + s, dirName)
+                elif mp4name == -1:
                     pass
-
+                
 if __name__ == "__main__":
     main()
